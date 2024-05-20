f143b53c2b7a6747d5af5919fae9d7563a6dec3b
Jihun <58257616+Lamyzm@users.noreply.github.com>
2024-05-20 17:40:13 +0900

    style: tailwind config 추가함. (#27)
    * style: tailwind config 추가함.
    
    * fix: 오타수정 / #26

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -22,34 +22,128 @@ const config = {
       screens: {
         desktop: '430px',
       },
+      backgroundImage: {
+        'evaluation-gradient': 'linear-gradient(90deg, #FFA3A3 0%, #FFCB96 35.5%, #FFE49D 65.5%, #77D276 99%',
+      },
+      fontSize: {
+        h1: [
+          '48px',
+          {
+            lineHeight: '48px',
+            fontWeight: '700',
+          },
+        ],
+        h2: [
+          '27px',
+          {
+            lineHeight: '35px',
+            fontWeight: '700',
+          },
+        ],
+        h3: [
+          '24px',
+          {
+            lineHeight: '36px',
+            fontWeight: '700',
+          },
+        ],
+        h4: [
+          '20px',
+          {
+            lineHeight: '28px',
+            fontWeight: '700',
+          },
+        ],
+        body1: [
+          '17px',
+          {
+            lineHeight: '24px',
+            fontWeight: '600',
+          },
+        ],
+        body2: [
+          '16px',
+          {
+            lineHeight: '24px',
+            fontWeight: '500',
+          },
+        ],
+        body3: [
+          '14px',
+          {
+            lineHeight: '20px',
+            fontWeight: '700',
+          },
+        ],
+        body4: [
+          '14px',
+          {
+            lineHeight: '20px',
+            fontWeight: '500',
+          },
+        ],
+        body5: [
+          '11px',
+          {
+            lineHeight: '18px',
+            fontWeight: '600',
+          },
+        ],
+        overline: [
+          '12px',
+          {
+            lineHeight: '18px',
+            fontWeight: '500',
+          },
+        ],
+        caption: [
+          '10px',
+          {
+            lineHeight: '18px',
+            fontWeight: '500',
+          },
+        ],
+      },
       colors: {
-        Primary: '#2563eb',
-        Background: '#f2f5fc',
-        'Container-bg': '#ffffff',
-        'Header-bg': '#ffffff',
-        Stroke: '#d1d5db',
-        'Text-disabled': '#d1d5db',
-        'Text-primary': '#111827',
-        'Text-secondary': '#6b7280',
-        'Primary-management': '#77D276',
-        'Secondary-management': '#CDF4CD',
-        'Primary-claim': '#FFB775',
-        'Secondary-claim': '#FFF0D0',
-        'Primary-facility': '#2563EB',
-        'Secondary-facility': '#E6EEFF',
-        'Primary-contract': '#67E8F9',
-        'Primary-rent': '#2563EB',
-        'Primary-empty': '#FFB775',
+        primary: '#2563eb',
+        background: '#f2f5fc',
+        'background-modal': '#111827',
+        'vontainer-bg': '#ffffff',
+        'header-bg': '#ffffff',
+        stroke: '#d1d5db',
+        'text-disabled': '#d1d5db',
+        'text-primary': '#111827',
+        'text-secondary': '#6b7280',
+        'icon-bg': '#f2f6ff',
+        'primary-management': '#77d276',
+        'secondary-management': '#cdf4cd',
+        'primay-claim': '#ffb775',
+        'secondary-claim': '#fff0d0',
+        'primay-facility': '#2563eb',
+        'secondary-facility': '#e6eeff',
+        'primary-rent': '#2563eb',
+        'primary-contract': '#67e8f9',
+        'primary-empty': '#ffb775',
+        'progress bar-step1': '#ffa3a3',
+        'progress bar-step2': '#ffe49d',
+        'progress bar-step3': '#22c55d',
+        'primary-badge-new': '#dc2626',
+        'green-50': '#F2FFF2',
+        'green-300': '#CDF4CD',
+        'green-500': '#22C55D',
+        'purple-50': '#FAF5FF',
+        'purple-100': '#f3e8ff',
+        'purple-400': '#c084fc',
+        'cyan-50': '#ECFEFF',
+        'cyan-300': '#67E8F9',
+        'cyan-400': '#22D3EE',
+        'orange-50': '#FFF0D0',
+        'orange-300': '#FFB775',
 
         border: 'hsl(var(--border))',
         input: 'hsl(var(--input))',
         ring: 'hsl(var(--ring))',
-        background: 'hsl(var(--background))',
         foreground: 'hsl(var(--foreground))',
-        primary: {
-          DEFAULT: 'hsl(var(--primary))',
-          foreground: 'hsl(var(--primary-foreground))',
-        },
         secondary: {
           DEFAULT: 'hsl(var(--secondary))',
           foreground: 'hsl(var(--secondary-foreground))',
@@ -76,6 +170,10 @@ const config = {
         },
       },
       borderRadius: {
+        container: '20px',
+        'desktop-stroke': '10px',
+        'mobile-graph': '10px',
+        'mobile-stroke': '8px',
         lg: 'var(--radius)',
         md: 'calc(var(--radius) - 2px)',
         sm: 'calc(var(--radius) - 4px)',
