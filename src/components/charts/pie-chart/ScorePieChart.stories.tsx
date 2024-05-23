8398804a5dfd64c912687a884a51a09500c8d595
wSeungMi <seungmi.dev@gmail.com>
2024-05-23 16:01:04 +0900

    add: ScorePieChart storybook 추가 #32

---
+++ src/components/charts/pie-chart/ScorePieChart.stories.tsx
@@ -0,0 +1,26 @@
+import { Meta, StoryObj } from '@storybook/react';
+import ScorePieChart from '@chart/pie-chart/ScorePieChart';
+
+const meta: Meta = {
+  title: 'chart/pieChart',
+  component: ScorePieChart,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof ScorePieChart>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+const dummyData = [
+  { name: '시설 점수', value: 45 },
+  { name: '관리 점수', value: 80 },
+  { name: '민원 점수', value: 30 },
+];
+
+export const PieChartCustom: Story = {
+  render: args => <ScorePieChart scoreData={dummyData} />,
+  args: {},
+};
