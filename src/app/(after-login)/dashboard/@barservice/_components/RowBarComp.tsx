496e775874b2f3536854f79e3e83d18a1ee52eb2
kano <ajvls98@gmail.com>
2024-05-24 01:56:02 +0900

    add: 대표호실 분석서비스 리펙터링

--- src/app/(after-login)/dashboard/@barservice/_components/RowBarComp.tsx
+++ src/app/(after-login)/dashboard/@barservice/_components/RowBarComp.tsx
@@ -3,49 +3,18 @@
 import React, { useEffect, useState } from 'react';
 import SingleCategoryHorizontalBarChart from '@chart/bar-chart/SingleCategoryHorizontalBarChart';
 import CommonChartLabel from '@Monocles/chart-label/CommonChartLabel';
+import { UseMediaQuery } from '@/hooks/index';
 
-const chartData = [
-  { name: 'January', rent: 100, price: 20 },
-  { name: 'January', rent: 20, price: 60 },
-  { name: 'January', rent: 80, price: 20 },
-];
-
-const Box = ({ chartData }: { chartData: any }) => {
-  const [size, setSize] = useState(34); // 기본 사이즈를 'small'로 설정합니다.
-
-  useEffect(() => {
-    // 브레이크 포인트 정의
-    const breakpoint = '(min-width: 430px)';
-    const mediaQuery = window.matchMedia(breakpoint);
-
-    // 브레이크 포인트에 따라 사이즈를 설정하는 함수
-    function handleBreakpointChange() {
-      if (mediaQuery.matches) {
-        setSize(28); // 브레이크 포인트 이상일 때
-      } else {
-        setSize(21); // 브레이크 포인트 미만일 때
-      }
-    }
-
-    // 초기 실행
-    handleBreakpointChange();
-
-    // 브레이크 포인트가 변경될 때마다 handleBreakpointChange 함수를 호출합니다.
-    mediaQuery.addEventListener('change', handleBreakpointChange);
-
-    // 컴포넌트 언마운트 시 이벤트 리스너를 제거합니다.
-    return () => {
-      mediaQuery.removeEventListener('change', handleBreakpointChange);
-    };
-  }, []);
+const Box = ({ chartData, text, accentColor }: { chartData: any; text: string; accentColor: string }) => {
+  const barChartSize = UseMediaQuery({ defaultSize: 27, changedSize: 21 });
 
   return (
     <div className="flex w-[295px] grow  flex-col rounded-box border-[0.74px] p-4 desktop:w-[394px] ">
-      <p className="mb-2 text-body1">임대료</p>
+      <p className="mb-2 text-body1">{text}</p>
       <div className="flex flex-row ">
         <div className="mr-2 flex flex-col justify-between py-[16px] desktop:py-2">
           <CommonChartLabel
-            boxColor={'primary'}
+            inlienBarColor={accentColor}
             size={'md'}
             textType={'default'}
             text="내호실"
@@ -59,10 +28,10 @@ const Box = ({ chartData }: { chartData: any }) => {
         </div>
         <div className="h-[90px] grow">
           <SingleCategoryHorizontalBarChart
-            size={size}
+            size={barChartSize}
             chartData={chartData}
             categoryKey="name"
-            accentColor="#2563EB"
+            accentColor={accentColor}
           />
         </div>
       </div>
@@ -70,16 +39,40 @@ const Box = ({ chartData }: { chartData: any }) => {
   );
 };
 
-export default function RowBarComp() {
+export default function RowBarComp({ chartData }: { chartData: object[]; isLoading: boolean }) {
+  useEffect(() => {
+    const awaitTime = new Promise<string>(resolve => {
+      setTimeout(() => {
+        resolve('hello');
+      }, 3000);
+    });
+
+    const fetchData = async () => {
+      const result = await awaitTime;
+    };
+
+    fetchData();
+  }, []);
+  const textArray = [
+    { title: '임대료', color: '#2461e6' },
+    { title: '재계약률', color: '#67e8f9' },
+    { title: '공실률', color: '#ffb775' },
+  ];
+
   return (
-    <div className="flex flex-col gap-6 desktop:flex-row">
-      {chartData.map((val, index) => {
-        return (
-          <Box
-            chartData={[val]}
-            key={index}></Box>
-        );
-      })}
+    <div>
+      <div className="flex flex-col gap-6 desktop:flex-row">
+        {chartData.map((val, index) => {
+          return (
+            <Box
+              key={index}
+              chartData={[val]}
+              text={textArray[index % textArray.length]['title']}
+              accentColor={textArray[index % textArray.length]['color']}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
--- src/app/(after-login)/dashboard/@barservice/page.tsx
+++ src/app/(after-login)/dashboard/@barservice/page.tsx
@@ -2,9 +2,23 @@ import { LocalIcon } from '@icon/index';
 import RoomBadge from '@Atoms/badge/RoomBadge';
 import RowBarComp from '@/app/(after-login)/dashboard/@barservice/_components/RowBarComp';
 
-export default function Page() {
+const chartData = [
+  { name: '임대', myRoom: 100, diff: 20 },
+  { name: '재계약률', myRoom: 20, diff: 60 },
+  { name: '공실률', myRoom: 80, diff: 20 },
+];
+
+export default async function Page() {
+  const awaitTime = new Promise<string>(resolve => {
+    setTimeout(() => {
+      resolve('hello');
+    }, 3000);
+  });
+
+  const result = await awaitTime;
+
   return (
-    <div className="flex w-[343px]  flex-col gap-3 rounded-container bg-white px-[24px] py-[32px] desktop:w-full desktop:px-[40px]">
+    <div className="flex w-[343px] flex-col  gap-3 rounded-container bg-white px-[24px] py-[32px] desktop:min-h-[250px] desktop:w-full desktop:px-[40px]">
       <div className="flex w-[240px] flex-row items-center justify-between">
         <p className=" text-h4">대표 호실 비교분석 서비스</p>
         <LocalIcon
@@ -16,7 +30,10 @@ export default function Page() {
         <RoomBadge roomName="미왕빌딩 A동 201호" />
       </div>
       <div>
-        <RowBarComp />
+        <RowBarComp
+          isLoading={false}
+          chartData={chartData}
+        />
       </div>
     </div>
   );
