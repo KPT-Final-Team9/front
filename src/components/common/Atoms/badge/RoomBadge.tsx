ee0b2816b98fcd220a64afa1fb2c0c993d79354a
kano <ajvls98@gmail.com>
2024-05-28 02:51:31 +0900

    fix: badge classname받을 수 있게 추가

--- src/components/common/Atoms/badge/RoomBadge.tsx
+++ src/components/common/Atoms/badge/RoomBadge.tsx
@@ -2,10 +2,15 @@ import React from 'react';
 
 import { Badge } from '@/components/ui/badge';
 import { LocalIcon } from '@icon/index';
+import { cn } from '@/lib/utils';
 
-export default function RoomBadge({ roomName }: { roomName: string }) {
+export default function RoomBadge({ roomName, className }: { roomName: string; className?: string }) {
   return (
-    <Badge className="item-center flex rounded-[10px] bg-blue-50 px-[10px] py-[4px] text-[14px] font-medium leading-[24px] text-primary">
+    <Badge
+      className={cn(
+        'item-center flex rounded-[10px] bg-blue-50 px-[10px] py-[4px] text-[14px] font-medium leading-[24px] text-primary',
+        className,
+      )}>
       <LocalIcon
         className="mr-[6px]"
         name="RoundStarIcon"
