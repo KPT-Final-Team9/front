0de93c8e17ebb34242873a94d1060fa17922cbac
kano <ajvls98@gmail.com>
2024-05-28 02:56:10 +0900

    fix: after-login 레이아웃 수정

--- src/app/(after-login)/layout.tsx
+++ src/app/(after-login)/layout.tsx
@@ -1,3 +1,10 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <section>{children}</section>;
+  return (
+    <div className="min-h-screen bg-background">
+      <div className="min-h-[87px] w-full bg-white">
+        <div className="mx-auto flex h-full max-w-[1440px]">hi</div>
+      </div>
+      <div className="desktopMaxW:p-0 mx-auto min-w-[359px] max-w-[1440px] px-4">{children}</div>
+    </div>
+  );
 }
