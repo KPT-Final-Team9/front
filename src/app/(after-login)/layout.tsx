379b754dd85c9fb38bfb3988d04e84044effe1f1
kano <ajvls98@gmail.com>
2024-05-28 02:59:07 +0900

    remove: 레이아웃 코드제거

--- src/app/(after-login)/layout.tsx
+++ src/app/(after-login)/layout.tsx
@@ -1,9 +1,6 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="min-h-screen bg-background">
-      <div className="min-h-[87px] w-full bg-white">
-        <div className="mx-auto flex h-full max-w-[1440px]">hi</div>
-      </div>
       <div className="desktopMaxW:p-0 mx-auto min-w-[359px] max-w-[1440px] px-4">{children}</div>
     </div>
   );
