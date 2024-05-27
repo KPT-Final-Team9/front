a854efdc38de7d5f13cd1de87c7a6d1cdfa31215
vangona <rlarhksrud12@naver.com>
2024-05-27 22:09:27 +0900

    refactor: 임대료 정보 컴포넌트 분리

--- src/app/(after-login)/room/@Overview/_component/ContractInfoClientComp.tsx
+++ src/app/(after-login)/room/@Overview/_component/ContractInfoClientComp.tsx
@@ -1,34 +1,14 @@
 'use client';
 import React, { useEffect, useState } from 'react';
 import dayjs from 'dayjs';
+import { calculateDateProgress } from '@/utils';
+import ContractRentInfo from './ContractRentInfo';
 
 const DUMMY_ROOM_NAME = '미왕빌딩 A동 201호';
 const DUMMY_DEPOSIT = 1000;
 const DUMMY_MONTHLY = 80;
 const DUMMY_START_DATE = '2023-05-07';
 const DUMMY_END_DATE = '2025-05-07';
-
-// 컴포넌트 분리할 수 있을 것 같아서 분리해서 작성함.
-export function ContractRentInfo({
-  roomDeposit,
-  roomMonthly,
-}: {
-  roomDeposit: number | undefined;
-  roomMonthly: number | undefined;
-}) {
-  return (
-    <div className="mb-6 mt-4 flex h-[62px] w-[212px] items-center justify-center gap-2 rounded-mobile-stroke bg-white desktop:rounded-desktop-stroke">
-      <div className="flex flex-col items-center">
-        <span className="text-overline text-primary">보증금</span>
-        <span className="text-h4">{roomDeposit ? roomDeposit : '-'} / </span>
-      </div>
-      <div className="flex flex-col items-center">
-        <span className="text-overline text-primary">임대료</span>
-        <span className="text-h4">{roomMonthly ? roomMonthly : '-'} 만 원</span>
-      </div>
-    </div>
-  );
-}
 
 export function ContractBadge() {
   return (
