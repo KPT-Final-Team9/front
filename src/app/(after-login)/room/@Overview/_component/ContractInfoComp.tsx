1a096be70222b569a3198f2bd0595e1cb6e2e3e6
vangona <rlarhksrud12@naver.com>
2024-05-27 21:31:18 +0900

    feat: 계약 정보 섹션 마크업

---
+++ src/app/(after-login)/room/@Overview/_component/ContractInfoComp.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function ContractInfoComp() {
+  return (
+    <div>
+      <h4 className="text-h4">계약 정보</h4>
+      <p className="text-body3"></p>
+      <div></div>
+      <div>
+        <span>계약 기간</span>
+        <div></div>
+      </div>
+    </div>
+  );
+}
