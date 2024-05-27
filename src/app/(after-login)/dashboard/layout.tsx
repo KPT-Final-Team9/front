7a64ec91ca96ac422ed45f4e722a00980be4d615
kano <ajvls98@gmail.com>
2024-05-28 02:57:07 +0900

    fix: dashboard  레이아웃 반응형

--- src/app/(after-login)/dashboard/layout.tsx
+++ src/app/(after-login)/dashboard/layout.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import DocumentStorageBox from '@/app/(after-login)/dashboard/_components/DocumentStorageBox';
 
 export default function Layout({
   MainEver,
@@ -12,23 +13,25 @@ export default function Layout({
   MainRoomSchedule: React.ReactNode;
 }) {
   return (
-    <div className="flex min-h-[100dvh] bg-background">
-      <main className="mx-auto flex max-w-[1440px] flex-col items-start justify-start pt-10">
-        <section className="flex  flex-col desktop:flex-row desktop:gap-14">
-          <article className="mx-auto mb-[30px] flex">{MainEver}</article>
-          <aside className="relative mb-[70px]  h-[400px] w-[336px] desktop:mb-[30px] desktop:block desktop:h-[700px]">
-            <div className="scrollbar-hide absolute h-[400px] w-[inherit] overflow-x-hidden overflow-y-scroll desktop:h-[700px]">
+    <>
+      <main className="flex flex-col items-center pt-10 desktop:justify-start ">
+        <section className="flex w-full flex-row max-[1330px]:flex-col desktop:gap-14">
+          <article className="mb-[30px] flex w-full">{MainEver}</article>
+          <aside className="relative mb-[70px] w-full desktop:mb-[30px] desktop:block desktop:h-[700px] min-[1330px]:max-w-[336px]">
+            <div className="scrollbar-hide overflow-y-scroll desktop:h-[700px] desktop:overflow-x-hidden">
               {RightSection}
             </div>
             <div className="sidebar-gradient absolute  bottom-0 h-0 w-full desktop:h-40"></div>
           </aside>
         </section>
-        <section className="z-20 mb-5 w-full">{MainRoomCompare}</section>
-        <section className="flex w-full flex-row gap-14">
-          <div className="flex-grow">{MainRoomSchedule}</div>
-          <div className="w-[336px] flex-none">{MainRoomSchedule}</div>
+        <section className=" mb-5 w-full desktop:w-full">{MainRoomCompare}</section>
+        <section className="flex w-full flex-col gap-14 desktop:w-full desktop:flex-row">
+          <article className="flex-grow ">{MainRoomSchedule}</article>
+          <article>
+            <DocumentStorageBox />
+          </article>
         </section>
       </main>
-    </div>
+    </>
   );
 }
