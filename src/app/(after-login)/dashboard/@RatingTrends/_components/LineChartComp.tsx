7565d28ed72aa5bc9baa0851c4526c69b2feae41
kano <ajvls98@gmail.com>
2024-05-28 11:31:07 +0900

    feat: 너무 심심해서 모션넣어봄

--- src/app/(after-login)/dashboard/@RatingTrends/_components/LineChartComp.tsx
+++ src/app/(after-login)/dashboard/@RatingTrends/_components/LineChartComp.tsx
@@ -1,8 +1,10 @@
+'use client';
 import React from 'react';
 import { NextIconButton } from '@Atoms/buttons/NextIconButton';
 import GradientChart from '@chart/GradientChart';
 import { BuildingRoomAvatar } from '@Atoms/avatar/BuildingRoomAvatar';
 import { StarIconButton } from '@Atoms/buttons/IconButtons';
+import { motion } from 'framer-motion';
 
 const chartData = [
   {
@@ -53,30 +55,34 @@ const chartData = [
 
 export default function LineChartComp({ mainRoom = false }: { mainRoom?: boolean }) {
   return (
-    <div className=" rounded-container bg-white py-[16px] pl-[23px] pr-[40px]">
-      <div className="mb-[24px] flex flex-row gap-1">
-        <BuildingRoomAvatar idx={0} />
-        <div className="flex grow flex-col">
-          <p className="text-[16px] text-gray-500">미왕빌딩</p>
-          <div className="flex justify-between">
-            <p className="text-body3">C동 601호</p>
+    <motion.div
+      whileHover={{ scale: 0.9 }}
+      transition={{ type: 'just', stiffness: 300 }}>
+      <div className=" rounded-container bg-white py-[16px] pl-[23px] pr-[40px]">
+        <div className="mb-[24px] flex flex-row gap-1">
+          <BuildingRoomAvatar idx={0} />
+          <div className="flex grow flex-col">
+            <p className="text-[16px] text-gray-500">미왕빌딩</p>
+            <div className="flex justify-between">
+              <p className="text-body3">C동 601호</p>
+            </div>
+          </div>
+          <div className="flex items-center p-[4px]">
+            <StarIconButton toggle={mainRoom} />
           </div>
         </div>
-        <div className="flex items-center p-[4px]">
-          <StarIconButton toggle={mainRoom} />
+        <div className="flex justify-between">
+          <GradientChart
+            data={chartData}
+            gradientColor="#D9E5FF"
+            strokeColor="#1D4ED8"
+          />
+          <NextIconButton
+            className="self-center"
+            shape={'square'}
+          />
         </div>
       </div>
-      <div className="flex justify-between">
-        <GradientChart
-          data={chartData}
-          gradientColor="#D9E5FF"
-          strokeColor="#1D4ED8"
-        />
-        <NextIconButton
-          className="self-center"
-          shape={'square'}
-        />
-      </div>
-    </div>
+    </motion.div>
   );
 }
--- src/app/(after-login)/dashboard/layout.tsx
+++ src/app/(after-login)/dashboard/layout.tsx
@@ -18,9 +18,7 @@ export default function Layout({
         <section className="flex w-full flex-row max-[1330px]:flex-col desktop:gap-14">
           <article className="mb-[30px] flex w-full">{Overview}</article>
           <aside className="relative mb-[70px] w-full desktop:mb-[30px] desktop:block desktop:h-[700px] min-[1330px]:max-w-[336px]">
-            <div className="scrollbar-hide overflow-y-scroll desktop:h-[700px] desktop:overflow-x-hidden">
-              {RatingTrends}
-            </div>
+            <div className="scrollbar-hide overflow-y-scroll desktop:h-[700px] ">{RatingTrends}</div>
             <div className="sidebar-gradient absolute  bottom-0 h-0 w-full desktop:h-40"></div>
           </aside>
         </section>
