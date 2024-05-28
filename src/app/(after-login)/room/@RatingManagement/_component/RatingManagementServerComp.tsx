8de0dff97a053bbff4ebf673c54eab37e78115f3
vangona <rlarhksrud12@naver.com>
2024-05-28 22:08:32 +0900

    feat: 내호실 평가 관리 서버컴포넌트 구현

--- src/app/(after-login)/room/@RatingManagement/_component/RatingManagementServerComp.tsx
+++ src/app/(after-login)/room/@RatingManagement/_component/RatingManagementServerComp.tsx
@@ -3,8 +3,8 @@ import RatingManagementClientComp from './RatingManagementClientComp';
 
 export default function RatingManagementServerComp() {
   return (
-    <div className="h-[222px] w-[343px] rounded-container bg-white desktop:h-[282px] desktop:w-[394px]">
-      RatingManagementServerComp
+    <div className="h-[222px] w-[343px] rounded-container bg-white px-6 py-4 desktop:h-[282px] desktop:w-[394px] desktop:px-10 desktop:py-8">
+      <h4 className="mb-8 text-h4 desktop:mb-10">내호실 평가 관리</h4>
       <RatingManagementClientComp />
     </div>
   );
