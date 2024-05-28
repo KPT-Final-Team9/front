4595e41ce13c153b60bc95869e82e5ed23a0bc92
vangona <rlarhksrud12@naver.com>
2024-05-28 17:14:26 +0900

    feat: formatDateToYYYY_MM_DD 유틸 함수 추가

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,5 @@
 import { FilterChartData } from '@/types/chart';
+import dayjs from 'dayjs';
 
 export const filterChartData: FilterChartData = ({ chartData, dataKey }) => {
   if (chartData.length === 0) return [];
@@ -18,4 +19,9 @@ export const calculateDateProgress = (start: Date, end: Date, current: Date | un
 
   // 3. 정수로 return
   return Math.round(progress); // 정수로 반올림
+};
+
+export const formatDateToYYYY_MM_DD = (date: Date | undefined): string => {
+  if (!date) return '----.--.--';
+  return dayjs(date).format('YYYY.MM.DD');
 };
