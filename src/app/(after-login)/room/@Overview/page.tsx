52781ddaeb31bed6222aaff9548eddf80b28a7d6
vangona <rlarhksrud12@naver.com>
2024-05-28 19:56:28 +0900

    feat: 개요 레이아웃 마크업

--- src/app/(after-login)/room/@Overview/page.tsx
+++ src/app/(after-login)/room/@Overview/page.tsx
@@ -5,8 +5,13 @@ export default function Page() {
   return (
     <>
       호실별 페이지 개요
-      <MyRoomScoreServerComp />
-      <ContractInfoServerComp />
+      <section className="flex">
+        <div className="flex flex-col gap-2 rounded-bl-container rounded-tl-container bg-white">
+          <ContractInfoServerComp />
+          <MyRoomScoreServerComp />
+        </div>
+        <div className="bg-white">{/* 임대 지표 비교 섹션 추가 필요 */}임대 지표 섹션</div>
+      </section>
     </>
   );
 }
