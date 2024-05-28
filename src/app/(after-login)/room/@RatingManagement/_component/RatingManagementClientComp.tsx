8daf8a2d760958f69ea3bea6c19381befcb070df
vangona <rlarhksrud12@naver.com>
2024-05-28 22:26:19 +0900

    feat: ToggleSwitch 합체

--- src/app/(after-login)/room/@RatingManagement/_component/RatingManagementClientComp.tsx
+++ src/app/(after-login)/room/@RatingManagement/_component/RatingManagementClientComp.tsx
@@ -1,8 +1,16 @@
 'use client';
+import ToggleSwitch from '@Atoms/switch/ToggleSwitch';
 import ReviewTrackProgress from '@Monocles/progress-bar/ReviewTrackProgress';
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function RatingManagementClientComp() {
+  const [isRatingActive, setIsRatingActive] = useState<boolean>(true);
+
+  const handleRatingActiveChange = () => {
+    setIsRatingActive(prev => !prev);
+    if (window) window.alert('호실 평가 변경됨');
+  };
+
   return (
     <div>
       <div className="mb-6 flex gap-4 desktop:mb-9 desktop:gap-9">
@@ -10,7 +18,12 @@ export default function RatingManagementClientComp() {
           <div className="text-body4 text-text-primary">내호실 평가받기 설정</div>
           <span className="text-overline text-text-secondary">내호실의 평가 내용을 요약하여 받아볼 수 있어요</span>
         </div>
-        <div>스위치</div>
+        <div>
+          <ToggleSwitch
+            isActive={isRatingActive}
+            handleToggleChange={handleRatingActiveChange}
+          />
+        </div>
       </div>
       <div className="flex flex-col">
         <p className="mb-2 text-body4">평가 진행률</p>
