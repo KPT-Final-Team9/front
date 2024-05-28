fe426b537409b53114eaba8fbec6acc95fe487b0
vangona <rlarhksrud12@naver.com>
2024-05-28 22:45:31 +0900

    feat: 평가 진행률 3단계 구현

--- src/components/common/Monocles/progress-bar/ReviewTrackProgress.tsx
+++ src/components/common/Monocles/progress-bar/ReviewTrackProgress.tsx
@@ -23,6 +23,21 @@ const ReviewTrackRank: { [key: string]: ProgressStep } = {
   },
 };
 
+// 평가 진행율을 이후 변경될 수 있는 값을 기준으로 3단계로 구분되며 정확한 수치는 보여지지 않음.
+type ReviewProgressGrade = 'FIRST_STEP' | 'SECOND_STEP' | 'THIRD_STEP';
+enum ReviewProgressGradeCriteria {
+  // 단계별 기준이 되는 수치가 변경될 경우 여기에서 수정하면 됨.
+  FIRST_STEP = 33.4,
+  SECOND_STEP = 66.7,
+  THIRD_STEP = 100,
+}
+
+const getGradeFromValue = (value: number): ReviewProgressGrade => {
+  if (value >= ReviewProgressGradeCriteria.SECOND_STEP) return 'THIRD_STEP';
+  if (value >= ReviewProgressGradeCriteria.FIRST_STEP) return 'SECOND_STEP';
+  return 'FIRST_STEP';
+};
+
 export default function ReviewTrackProgress({
   value,
   trackFontClass,
@@ -33,10 +48,12 @@ export default function ReviewTrackProgress({
   legendClass?: string;
 }) {
   const reviewTrackFontClass = cn('text-caption', trackFontClass);
+  const progressGrade: ReviewProgressGrade = getGradeFromValue(value);
+
   return (
     <>
       <ReverseProgress
-        value={value}
+        value={ReviewProgressGradeCriteria[progressGrade]}
         className="h-[13px]"
         indicatorColor="evaluation-gradient"
       />
