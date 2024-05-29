7395d07af4fdd0b3a74848b015ab861eb32002f7
kano <ajvls98@gmail.com>
2024-05-30 01:56:37 +0900

    feat: 임대지표비교 섹션 추가

--- src/app/(after-login)/room/@Overview/page.tsx
+++ src/app/(after-login)/room/@Overview/page.tsx
@@ -1,16 +1,19 @@
 import ContractInfoServerComp from '@/app/(after-login)/room/@Overview/_component/ContractInfoServerComp';
 import MyRoomScoreServerComp from '@/app/(after-login)/room/@Overview/_component/MyRoomScoreServerComp';
+import RatingCompareComp from '@/app/(after-login)/room/@Overview/_component/RatingCompareComp';
 
 export default function Page() {
   return (
     <>
       호실별 페이지 개요
-      <section className="flex">
+      <section className="flex flex-col rounded-container bg-white desktop:flex-row">
         <div className="flex flex-col gap-2 rounded-bl-container rounded-tl-container bg-white">
           <ContractInfoServerComp />
           <MyRoomScoreServerComp />
         </div>
-        <div className="bg-white">{/* 임대 지표 비교 섹션 추가 필요 */}임대 지표 섹션</div>
+        <div className="w-full rounded-container">
+          <RatingCompareComp />
+        </div>
       </section>
     </>
   );
