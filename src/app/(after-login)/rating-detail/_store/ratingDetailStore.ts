68d46aed1fc2873bd5280bf341d7a45df940d790
vangona <rlarhksrud12@naver.com>
2024-05-30 15:24:15 +0900

    feat: ratingDetail 페이지 store 생성

---
+++ src/app/(after-login)/rating-detail/_store/ratingDetailStore.ts
@@ -0,0 +1,18 @@
+import { create } from 'zustand';
+
+export enum ViewState {
+  TOTAL = 'total',
+  BOOKMARK = 'bookmark',
+}
+
+interface RatingDetailStore {
+  viewState: ViewState;
+  setTotalView: () => void;
+  setBookmarkView: () => void;
+}
+
+export const useRatingDetailStore = create<RatingDetailStore>(set => ({
+  viewState: ViewState.TOTAL,
+  setTotalView: () => set({ viewState: ViewState.TOTAL }),
+  setBookmarkView: () => set({ viewState: ViewState.BOOKMARK }),
+}));
