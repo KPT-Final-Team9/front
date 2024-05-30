94eed6812e83618fa9dfdef42890ab881de1e918
vangona <rlarhksrud12@naver.com>
2024-05-30 17:37:39 +0900

    feat: PopoverTrigger 구현

--- src/components/common/Monocles/popover-trigger/Popover.stories.tsx
+++ src/components/common/Monocles/popover-trigger/Popover.stories.tsx
@@ -2,6 +2,7 @@ import { Meta, StoryObj } from '@storybook/react';
 import { Popover as PopoverComp } from '@/components/ui/popover';
 import PopoverTrigger from './PopoverTrigger';
 import PopoverContent from './PopoverContent';
+import { LocalIcon } from '@icon/index';
 
 const meta: Meta = {
   title: 'Atoms/Popover/Popover',
@@ -17,7 +18,16 @@ export default meta;
 export const Popover: StoryObj<typeof PopoverComp> = {
   render: ({ ...args }) => (
     <PopoverComp>
-      <PopoverTrigger>Popover Trigger</PopoverTrigger>
+      <PopoverTrigger
+        icon={
+          <LocalIcon
+            width={20}
+            height={20}
+            name="FilterIcon"
+          />
+        }
+        label="Trigger"
+      />
       <PopoverContent>Popover Content 테스트</PopoverContent>
     </PopoverComp>
   ),
--- src/components/common/Monocles/popover-trigger/PopoverTrigger.tsx
+++ src/components/common/Monocles/popover-trigger/PopoverTrigger.tsx
@@ -3,10 +3,23 @@ import { cn } from '@/lib/utils';
 import * as PopoverPrimitive from '@radix-ui/react-popover';
 import React from 'react';
 
-export default function PopoverTrigger({ className, ...props }: PopoverPrimitive.PopoverTriggerProps) {
+export default function PopoverTrigger({
+  className,
+  isActive,
+  icon,
+  label,
+  ...props
+}: PopoverPrimitive.PopoverTriggerProps & { isActive?: boolean; icon?: React.ReactNode; label?: React.ReactNode }) {
   return (
     <PopoverTriggerComp
-      className={cn('border', className)}
-      {...props}></PopoverTriggerComp>
+      className={cn(
+        'flex items-center gap-2 rounded-mobile-stroke border bg-white px-[10px] py-1 text-body1 text-text-primary active:scale-[.97] desktop:rounded-desktop-stroke desktop:py-2',
+        { ['border-primary']: isActive, ['bg-blue-50']: isActive },
+        className,
+      )}
+      {...props}>
+      {icon}
+      <div>{label}</div>
+    </PopoverTriggerComp>
   );
 }
