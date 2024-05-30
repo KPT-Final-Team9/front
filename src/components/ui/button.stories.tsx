93e31cf61dbd8a2556f47b033262297fcab853dc
vangona <rlarhksrud12@naver.com>
2024-05-30 21:25:05 +0900

    feat: button story 보완

--- src/components/ui/button.stories.tsx
+++ src/components/ui/button.stories.tsx
@@ -30,7 +30,36 @@ export const Default: Story = {
     className: '',
     variant: 'default',
     size: 'sm',
-    colors: 'mono',
+    colors: 'default',
     children: '로그아웃',
   },
+};
+
+export const AllSizeButtons: Story = {
+  render: () => (
+    <div className="flex flex-col gap-2">
+      <Button size="default">Default (Medium)</Button>
+      <Button size="icon">icon</Button>
+      <Button size="sm">Small</Button>
+      <Button size="md">Medium (Default)</Button>
+      <Button size="lg">Large</Button>
+    </div>
+  ),
+};
+
+export const AllVariantButtons: Story = {
+  render: () => (
+    <div className="flex flex-col gap-2">
+      <Button variant="default">Default (Primary)</Button>
+      <Button variant="destructive">Destructive</Button>
+      <Button variant="ghost">Ghost</Button>
+      <Button variant="icon">Icon</Button>
+      <Button variant="link">Link</Button>
+      <Button variant="none">None</Button>
+      <Button variant="orange">Orange</Button>
+      <Button variant="outline">Outline</Button>
+      <Button variant="primary">Primary (Default)</Button>
+      <Button variant="secondary">Secondary</Button>
+    </div>
+  ),
 };
