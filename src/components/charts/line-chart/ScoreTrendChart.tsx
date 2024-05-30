39ed5a4bd4967b40f29dc5e671a5d6fd0b6c5319
kano <ajvls98@gmail.com>
2024-05-30 14:13:46 +0900

    feat: 히스토그램 그래프 구현

--- src/components/charts/line-chart/ScoreTrendChart.tsx
+++ src/components/charts/line-chart/ScoreTrendChart.tsx
@@ -1,12 +1,12 @@
 'use client';
 import React from 'react';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
-import { ScoreTrendChartCustomTooltip } from '@chart/CustomTooltip';
+import { ScoreTrendLineChartCustomTooltip } from '@chart/CustomTooltip';
 import { filterChartData } from '@/utils';
 
 export default function ScoreTrendChart({ dateKey, chartData }: { dateKey: string; chartData: any }) {
   const tickStyle = { fill: '#9b9b9b', fontSize: 14, fontWeight: 400 };
-  // 기준 x 축 키값 제외한 키 필터
+  // 기준 x 축, 키값 제외한 키 필터
   const filteredChartData = filterChartData({ chartData, dataKey: dateKey });
   return (
     <div className="my-2 h-[275px] w-full">
@@ -40,7 +40,7 @@ export default function ScoreTrendChart({ dateKey, chartData }: { dateKey: strin
             orientation="right"
           />
           <Tooltip
-            content={<ScoreTrendChartCustomTooltip />}
+            content={<ScoreTrendLineChartCustomTooltip />}
             cursor={{ fill: 'black' }}
           />
           <Line
