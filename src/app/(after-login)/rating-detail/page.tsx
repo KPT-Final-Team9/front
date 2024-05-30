825a68091513e4230463fb5e00f175c789277c09
vangona <rlarhksrud12@naver.com>
2024-05-30 16:35:13 +0900

    feat: RatingDetail searchInput 구현

--- src/app/(after-login)/rating-detail/page.tsx
+++ src/app/(after-login)/rating-detail/page.tsx
@@ -1,15 +1,18 @@
 import RatingDetailDataTable from '@/app/(after-login)/rating-detail/_component/RatingDetailDataTable';
 import BookmarkButtons from '@/app/(after-login)/rating-detail/_component/BookmarkButtons';
+import RatingDeatilSearchInput from '@/app/(after-login)/rating-detail//_component/RatingDeatilSearchInput';
 
 export default function Page() {
   return (
     <section className="w-full">
-      <div className="flex flex-grow flex-col-reverse justify-between desktop:flex-row">
+      <div className="flex flex-grow flex-col-reverse justify-between gap-4 desktop:flex-row">
         <div className="mb-8 flex w-full justify-between">
           <BookmarkButtons />
           <div className="desktop:display-block display-hidden">옵션 선택</div>
         </div>
-        <div className="flex-shrink-0 desktop:ml-12">검색어 입력</div>
+        <div className="flex-shrink-0 desktop:ml-8">
+          <RatingDeatilSearchInput />
+        </div>
       </div>
       <div className="mb-10">
         <RatingDetailDataTable />
