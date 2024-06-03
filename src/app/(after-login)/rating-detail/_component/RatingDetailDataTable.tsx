7d8efb68314d4e57b24a7062622f9e2a5a8b3e30
vangona <rlarhksrud12@naver.com>
2024-06-04 01:11:50 +0900

    feat: Detail Dialog 구현

--- src/app/(after-login)/rating-detail/_component/RatingDetailDataTable.tsx
+++ src/app/(after-login)/rating-detail/_component/RatingDetailDataTable.tsx
@@ -30,6 +30,9 @@ import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@
 import { formatDateToYYYY_MM_DD } from '@/utils';
 import { BookmarkIcon } from '@/asset/Icons';
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@/components/ui/dialog';
+import { BuildingRoomAvatar } from '@Atoms/avatar/BuildingRoomAvatar';
+import { Button } from '@/components/ui/button';
+import { LocalIcon } from '@icon/index';
 
 enum RatingCategory {
   FACILITY = '시설 평가',
@@ -376,11 +379,55 @@ export default function RatingDetailDataTable() {
       <Dialog
         open={isDialogOpen}
         onOpenChange={setIsDialogOpen}>
-        <DialogContent>
-          <DialogHeader>
-            <DialogTitle>평가 내용 상세보기</DialogTitle>
+        <DialogContent className="p-0">
+          <DialogHeader className="h-20 justify-center px-10 py-6">
+            <DialogTitle className="text-h4 text-text-primary">평가 내용 상세보기</DialogTitle>
           </DialogHeader>
-          {selectedRow?.getValue(RatingDetailTableAccessorKey.CONTENT)}
+          <div className="flex gap-3 border-b border-t border-stroke bg-blue-50 px-10 py-6">
+            <BuildingRoomAvatar idx={0} />
+            <div className="text-h3 text-primary">
+              {selectedRow?.getValue(RatingDetailTableAccessorKey.BUILDING)}
+              {selectedRow?.getValue(RatingDetailTableAccessorKey.ROOM)}
+            </div>
+            <Button
+              variant="icon"
+              className="p-1">
+              <LocalIcon
+                name="BookmarkIcon"
+                width={24}
+                height={24}
+                fill={selectedRow?.getValue(RatingDetailTableAccessorKey.IS_BOOKMARK) ? 'black' : 'none'}
+              />
+            </Button>
+          </div>
+          <div className="px-10 pb-16 pt-6">
+            <div className="mb-16 flex w-full flex-col items-center justify-center">
+              <div className="mb-2 text-body2 text-text-secondary">평가점수</div>
+              <div className="text-h1 text-text-primary">
+                {selectedRow?.getValue(RatingDetailTableAccessorKey.SCORE)}점
+              </div>
+            </div>
+            <div className="flex flex-col gap-3">
+              <div className="flex">
+                <div className="w-[140px] text-body1 text-text-secondary">유형</div>
+                <div className="text-h4 text-text-primary">
+                  {selectedRow?.getValue(RatingDetailTableAccessorKey.CATEGORY)}
+                </div>
+              </div>
+              <div className="flex">
+                <div className="w-[140px] text-body1 text-text-secondary">평가 날짜</div>
+                <div className="text-h4 text-text-primary">
+                  {formatDateToYYYY_MM_DD(selectedRow?.getValue(RatingDetailTableAccessorKey.RATING_DATE))}
+                </div>
+              </div>
+              <div className="flex flex-col gap-3">
+                <div className="w-[140px] text-body1 text-text-secondary">평가 내용</div>
+                <div className="text-h4 text-text-primary">
+                  {selectedRow?.getValue(RatingDetailTableAccessorKey.CONTENT)}
+                </div>
+              </div>
+            </div>
+          </div>
         </DialogContent>
       </Dialog>
       <div className="rounded-container border bg-white">
