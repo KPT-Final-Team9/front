ccc6a2e9282550a237f4068bb2fea4210ade278f
wSeungMi <seungmi.dev@gmail.com>
2024-06-04 02:31:56 +0900

    style: 평가 완료 페이지 구현 및 애니메이션 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -194,10 +194,18 @@ const config = {
           from: { height: 'var(--radix-accordion-content-height)' },
           to: { height: '0' },
         },
+        flash: {
+          '25%, 40%': { opacity: '0' },
+          '50%': { opacity: '1' },
+          '75%': { opacity: '0' },
+        },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
+        'flash-first': 'flash 2s infinite',
+        'flash-two': 'flash 3s infinite',
+        'flash-three': 'flash 1.8s infinite',
       },
     },
   },
