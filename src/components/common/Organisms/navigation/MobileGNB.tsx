dd056c42aca85cb1bf331eb1fd04e2d074905cda
wSeungMi <seungmi.dev@gmail.com>
2024-06-05 00:51:19 +0900

    feat: mobile gnb 레이아웃 및 라우팅 설정

--- src/components/common/Organisms/navigation/MobileGNB.tsx
+++ src/components/common/Organisms/navigation/MobileGNB.tsx
@@ -1,12 +1,73 @@
+'use client';
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
 import { cn } from '@/lib/utils';
+import { LocalIcon } from '@icon/index';
+import { Button } from '@/components/ui/button';
+
+// DesktopGNB과 동일하게 작성
+export enum NAV_TYPE {
+  DASH_BOARD = 'dashboard',
+  ROOM_DETAIL = 'room',
+  EVALUATION = 'evaluation',
+}
+
+export type NavItemsType = Record<
+  NAV_TYPE,
+  { path: string; label: string; icon: 'NavDashboardIcon' | 'NavRoomIcon' | 'NavRatingDetailIcon' }
+>;
+
+const NAV_ITEMS: NavItemsType = {
+  [NAV_TYPE.DASH_BOARD]: { path: '/dashboard', label: '대시보드', icon: 'NavDashboardIcon' },
+  [NAV_TYPE.ROOM_DETAIL]: { path: '/room', label: '호실관리', icon: 'NavRoomIcon' },
+  [NAV_TYPE.EVALUATION]: { path: '/rating-detail', label: '평가상세', icon: 'NavRatingDetailIcon' },
+};
 
 export default function MobileGNB() {
   return (
-    <nav
-      className={cn(
-        'h-gnb fixed bottom-0 left-0 right-0 z-40 flex w-full border-t border-stroke bg-white desktop:hidden',
-      )}>
-      네비
+    <nav className={cn('h-gnb fixed bottom-0 left-0 right-0 z-40 border-t border-stroke bg-white px-7 desktop:hidden')}>
+      <NavListComp />
     </nav>
+  );
+}
+
+function NavListComp() {
+  const pathname = usePathname();
+  const navListActive = 'fill-primary text-primary';
+
+  return (
+    <ul className="h-gnb flex min-w-[320px] items-center justify-between gap-7 text-body5">
+      {Object.keys(NAV_ITEMS).map(key => {
+        const item = key as NAV_TYPE;
+
+        return (
+          <Link
+            key={NAV_ITEMS[item].path}
+            href={NAV_ITEMS[item].path}
+            className="flex flex-col items-center gap-y-[6px]">
+            <LocalIcon
+              name={NAV_ITEMS[item].icon}
+              width={24}
+              height={24}
+            />
+            <li>{NAV_ITEMS[item].label}</li>
+          </Link>
+        );
+      })}
+      <li>
+        <Button
+          variant={'icon'}
+          className="flex flex-col items-center gap-y-[6px] px-0 text-body5">
+          <LocalIcon
+            name={'NavAccountIcon'}
+            width={24}
+            height={24}
+          />
+          내계정
+        </Button>
+      </li>
+    </ul>
   );
 }
