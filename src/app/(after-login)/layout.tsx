ed1b4673acf1cd7f7c3d2b4d10a559dd2b8d42f2
wSeungMi <seungmi.dev@gmail.com>
2024-06-04 22:17:28 +0900

    add: 브라우저 사이즈에 따른 GNB 반응형 렌더링 조건 추가

--- src/app/(after-login)/layout.tsx
+++ src/app/(after-login)/layout.tsx
@@ -1,9 +1,11 @@
 import DesktopGNB from '@Organisms/navigation/DesktopGNB';
+import MobileGNB from '@Organisms/navigation/MobileGNB';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="min-h-screen bg-background">
       <DesktopGNB />
+      <MobileGNB />
       <div className="mx-auto min-w-[359px] max-w-[1440px] px-4 desktopMaxW:p-0">{children}</div>
     </div>
   );
--- src/components/common/Organisms/navigation/DesktopGNB.tsx
+++ src/components/common/Organisms/navigation/DesktopGNB.tsx
@@ -35,7 +35,7 @@ export default function DesktopGNB() {
 
   return (
     <>
-      <nav className={cn('top-0 z-40 flex w-full bg-white p-[16px] desktop:px-5', GNB_HEIGHT)}>
+      <nav className={cn('top-0 z-40 hidden w-full bg-white p-[16px] desktop:flex desktop:px-5', GNB_HEIGHT)}>
         <div className="mx-auto flex h-full w-[1440px] items-center justify-between">
           <div className={cn(' flex items-center gap-4', GNB_HEIGHT)}>
             <LocalIcon
