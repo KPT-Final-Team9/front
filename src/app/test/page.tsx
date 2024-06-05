54c1d1b30e9ed81c5494b3135ae256e9d8afab86
kano <ajvls98@gmail.com>
2024-06-05 12:41:53 +0900

    text 커밋입니다 추후 삭제

---
+++ src/app/test/page.tsx
@@ -0,0 +1,10 @@
+import axios from 'axios';
+export default async function Page() {
+  const res = await axios.get('http://localhost:3000/api/mock');
+  console.log(res);
+  return (
+    <div>
+      <p>{res.data.hello}</p>
+    </div>
+  );
+}
