65b99d82030765679b32de5cf4089f6dc99835a0
kano <ajvls98@gmail.com>
2024-06-05 12:59:12 +0900

    test 커밋입니다

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,10 +1,14 @@
 import axios from 'axios';
 export default async function Page() {
-  const res = await axios.get('http://localhost:3000/api/mock');
-  console.log(res);
-  return (
-    <div>
-      <p>{res.data.hello}</p>
-    </div>
-  );
+  try {
+    const res = await axios.get('http://localhost:3000/api/mock');
+    console.log(res);
+    return (
+      <div>
+        <p>{res.data.hello}</p>
+      </div>
+    );
+  } catch (err) {
+    err;
+  }
 }
