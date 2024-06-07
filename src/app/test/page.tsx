8fe91f9f2ceb4800c9881347b59a9a3b34980669
kano <ajvls98@gmail.com>
2024-06-07 12:53:44 +0900

    doc: 에러 주석추가

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -12,6 +12,12 @@ export default async function Page() {
     // 문제는 여기가 아니라 return fetch 라이브러리나
     // @/contexts/AuthProvider 쪽에있는 useSession 쪽에서 문제가 발생하는 것 같습니다
 
+    //log:
+    // ClientFetchError: Unexpected token '토', "토큰이 비어 있습니다." is not valid JSON
+    // at fetchData (client.js:49:22)
+    // at async getSession (react.js:109:21)
+    // at async __NEXTAUTH._getSession (react.js:272:43)
+
     const { data: session, status } = useSession();
     console.log('session');
     console.log(session);
