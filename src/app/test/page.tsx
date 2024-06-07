af7101e7632588aa382d3493f242b706838fe323
kano <ajvls98@gmail.com>
2024-06-07 12:59:07 +0900

    doc: authjs 에러부분 주석추가

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,10 +1,11 @@
+'use client';
 import { useSession } from 'next-auth/react';
 import { auth } from '@/auth';
 import { CustomSession } from '@/types/auth';
-export default async function Page() {
+export default function Page() {
   try {
-    const userInfo = (await auth()) as CustomSession;
-    console.log(userInfo);
+    // const userInfo = (await auth()) as CustomSession;
+    // console.log('terminal log!', userInfo);
     // const res = await axios.get('http://localhost:3000/api/mock');
     // console.log(res);
 
