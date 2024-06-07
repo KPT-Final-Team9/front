256e7f30fa5588e2b54755fc75ae9edb9954dc87
kano <ajvls98@gmail.com>
2024-06-07 12:46:10 +0900

    test: 테스트 페이지

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,13 +1,19 @@
-import axios from 'axios';
+import { useSession } from 'next-auth/react';
+import { auth } from '@/auth';
+import { CustomSession } from '@/types/auth';
 export default async function Page() {
   try {
-    const res = await axios.get('http://localhost:3000/api/mock');
-    console.log(res);
-    return (
-      <div>
-        <p>{res.data.hello}</p>
-      </div>
-    );
+    const userInfo = (await auth()) as CustomSession;
+    console.log(userInfo);
+    // const res = await axios.get('http://localhost:3000/api/mock');
+    // console.log(res);
+    const { data: session, status } = useSession();
+    if (status === 'authenticated') {
+      return <p>Signed in as {session.user.email}</p>;
+    }
+    console.log(session);
+    console.log('session');
+    return <div>{/* <p>{res.data.hello}</p> */}</div>;
   } catch (err) {
     err;
   }
