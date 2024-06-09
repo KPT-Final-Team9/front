a3e6fd7f32484704cfad726d9dc637f7021d1f30
kano <ajvls98@gmail.com>
2024-06-09 17:18:22 +0900

    remove: 이전 코드 삭제

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,58 +1,7 @@
 import { apisBaseUrl, logRequestInterceptor, publicApiBaseUrl } from '@/services/intercepter';
 import { getAuth } from '@/serverActions/index';
 
-// async function createApiInstance(headers) {
-//   let baseUrl = '/';
-//   let headersList = null;
-
-//   if (typeof window === 'undefined') {
-//     // 서버 사이드에서 실행될 때
-//     const { headers: serverHeaders } = await import('next/headers');
-//     headersList = serverHeaders();
-//     const host = headersList.get('host');
-//     if (host) {
-//       baseUrl = host;
-//     }
-//   } else {
-//     baseUrl = '/';
-//   }
-
-//   return {
-//     baseUrl,
-//     headers,
-//   };
-// }
-
-// // /api
-// export async function baseApis() {
-//   const userInfo = await getAuth();
-
-//   const apiInstanceConfig = await createApiInstance({
-//     Accept: 'application/json',
-//     'Content-Type': 'application/json',
-//     Authorization: `Bearer ${userInfo.token}`,
-//   });
-
-//   return logRequestInterceptor({
-//     fetch: apisBaseUrl({
-//       ...apiInstanceConfig,
-//     }),
-//   });
-// }
-
-// // public-api
-// export async function basePublicApi() {
-//   const publicApiInstanceConfig = await createApiInstance({
-//     Accept: 'application/json',
-//     'Content-Type': 'application/json',
-//   });
-//   return publicApiBaseUrl({
-//     fetch: logRequestInterceptor({
-//       ...publicApiInstanceConfig,
-//     }),
-//   });
-// }
-
+/*  /api */
 export async function baseApis() {
   let baseUrl = process.env.HOST_URL;
   let headersList = null;
@@ -85,6 +34,7 @@ export async function baseApis() {
   });
 }
 
+/*/public-api */
 export async function basePublicApi() {
   let baseUrl = process.env.HOST_URL;
   let headersList = null;
