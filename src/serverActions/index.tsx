1fbebc26a5088923d356e10611637b36cc9dd677
kano <ajvls98@gmail.com>
2024-06-09 17:33:40 +0900

    add: 서버액션 로그 추가

--- src/serverActions/index.tsx
+++ src/serverActions/index.tsx
@@ -13,5 +13,12 @@ export async function wait(duration = 1000): Promise<{ message: string }> {
 
 // 클라이언트 사이드에서 auth 정보 취득용
 export async function getAuth() {
-  return (await auth()) as CustomSession;
+  try {
+    const session = (await auth()) as CustomSession;
+    console.log('Authenticated session:', session);
+    return session;
+  } catch (error) {
+    console.error('Authentication error:', error);
+    throw error;
+  }
 }
