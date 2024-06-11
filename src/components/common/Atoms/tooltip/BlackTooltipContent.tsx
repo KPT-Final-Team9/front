7a92127811e3c4564e7e864b1c47e5960127e50a
vangona <rlarhksrud12@naver.com>
2024-06-11 19:28:08 +0900

    feat: 임대 지표 비교 툴팁 구현

--- src/components/common/Atoms/tooltip/BlackTooltipContent.tsx
+++ src/components/common/Atoms/tooltip/BlackTooltipContent.tsx
@@ -10,8 +10,9 @@ export default function BlackTooltipContent({ title, ...props }: TooltipContentP
     <TooltipContent
       {...props}
       align={props.align ?? 'start'}
+      alignOffset={-20}
       className={cn(
-        'flex max-w-[220px] flex-col gap-1 rounded-container border-black bg-black px-4 py-3 text-overline text-white desktop:max-w-[442px] desktop:gap-2 desktop:px-8 desktop:py-4 desktop:text-body2',
+        'flex max-w-[220px] flex-col gap-1 rounded-[20px] border-black bg-black px-4 py-3 text-overline text-white desktop:max-w-[442px] desktop:gap-2 desktop:px-8 desktop:py-4 desktop:text-body2',
         props.className,
       )}>
       {title && (
