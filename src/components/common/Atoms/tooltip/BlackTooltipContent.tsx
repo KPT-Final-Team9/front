5e08485e5befb1d05f7b4400cb32cdf138052bd4
vangona <rlarhksrud12@naver.com>
2024-06-11 19:07:25 +0900

    feat: 대시보드 툴팁 구현

--- src/components/common/Atoms/tooltip/BlackTooltipContent.tsx
+++ src/components/common/Atoms/tooltip/BlackTooltipContent.tsx
@@ -11,7 +11,7 @@ export default function BlackTooltipContent({ title, ...props }: TooltipContentP
       {...props}
       align={props.align ?? 'start'}
       className={cn(
-        'flex flex-col gap-1 border-black bg-black px-4 py-3 text-overline text-white desktop:gap-2 desktop:px-8 desktop:py-4 desktop:text-body2',
+        'flex max-w-[220px] flex-col gap-1 rounded-container border-black bg-black px-4 py-3 text-overline text-white desktop:max-w-[442px] desktop:gap-2 desktop:px-8 desktop:py-4 desktop:text-body2',
         props.className,
       )}>
       {title && (
