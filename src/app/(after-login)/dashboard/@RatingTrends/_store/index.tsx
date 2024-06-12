66ab1d04d9554313b1fbd7a06067227812c999b1
kano <ajvls98@gmail.com>
2024-06-12 16:46:16 +0900

    feat: 그라디언트 라인차트 api연동

--- src/app/(after-login)/dashboard/@RatingTrends/_store/index.tsx
+++ src/app/(after-login)/dashboard/@RatingTrends/_store/index.tsx
@@ -2,28 +2,34 @@ import { create } from 'zustand';
 
 type MainModalStore = {
   roomId: number | undefined;
+  roomName: string | undefined;
   setRoomId: (id: number | undefined) => void;
+  setRoomName: (id: string | undefined) => void;
   ModalState: boolean;
   setModal: (val: boolean) => void;
 };
 
 const useMainModalStore = create<MainModalStore>()(set => ({
   roomId: undefined,
+  roomName: undefined,
   ModalState: false,
   setRoomId: (id: number | undefined) => {
     set(() => ({ roomId: id }));
+  },
+  setRoomName: (name: string | undefined) => {
+    set(() => ({ roomName: name }));
   },
   setModal: val => {
     set({ ModalState: val });
   },
 }));
 
-type CompleteModalStore = {
+type StateModalStore = {
   modalState: boolean;
   setModal: (val: boolean) => void;
 };
 
-const useCompleteModalStore = create<CompleteModalStore>(set => ({
+const useCompleteModalStore = create<StateModalStore>(set => ({
   modalState: false,
   setModal: val => {
     set(state => {
@@ -34,4 +40,31 @@ const useCompleteModalStore = create<CompleteModalStore>(set => ({
     });
   },
 }));
-export { useMainModalStore, useCompleteModalStore };
+
+const useFailedModalStore = create<StateModalStore>(set => ({
+  modalState: false,
+  setModal: val => {
+    set(state => {
+      if (state.modalState !== val) {
+        return { modalState: val };
+      }
+      return state;
+    });
+  },
+}));
+
+type RepresentRoomStore = {
+  representRoomId: number | undefined;
+  setRepresentRoom: (val: number | undefined) => void;
+};
+
+const useRepresentRoomStore = create<RepresentRoomStore>(set => ({
+  representRoomId: undefined,
+  setRepresentRoom: val => {
+    set(state => ({
+      representRoomId: val,
+    }));
+  },
+}));
+
+export { useMainModalStore, useCompleteModalStore, useFailedModalStore, useRepresentRoomStore };
--- src/app/(after-login)/dashboard/@RatingTrends/page.tsx
+++ src/app/(after-login)/dashboard/@RatingTrends/page.tsx
@@ -1,36 +1,68 @@
-'use client';
 import LineChartComp from '@/app/(after-login)/dashboard/@RatingTrends/_components/LineChartComp';
 import {
   MainRoomSetting,
   MainRoomSettingComplete,
+  MainRoomSettingFailed,
 } from '@/app/(after-login)/dashboard/@RatingTrends/_components/Modal';
+import { dashboardPageType } from '@/types/common/pageTypes';
+import { QueryOptions } from '@/constants/index';
+import { fetchJsonData } from '@/services/api';
 
-export default function Page() {
-  return (
-    //메인 룸은 true false 로 결정된다고 함.
+interface AvgByMonth {
+  selected_month: string;
+  total_avg: number;
+  evaluation_progress: number;
+  facility_avg: number;
+  management_avg: number;
+  complaint_avg: number;
+}
 
-    <>
-      <div className="flex flex-row gap-6 after:h-4 after:content-[''] desktop:flex-col">
-        <LineChartComp
-          mainRoom={true}
-          roomId={1}
-        />
-        <LineChartComp roomId={2}></LineChartComp>
-        <LineChartComp></LineChartComp>
-        <LineChartComp></LineChartComp>
-        <LineChartComp></LineChartComp>
-        <LineChartComp></LineChartComp>
-        <LineChartComp></LineChartComp>
-        <LineChartComp></LineChartComp>
-        <LineChartComp></LineChartComp>
-        <LineChartComp></LineChartComp>
-        <LineChartComp></LineChartComp>
-        <LineChartComp></LineChartComp>
-        <LineChartComp></LineChartComp>
-        <LineChartComp></LineChartComp>
-      </div>
-      <MainRoomSetting />
-      <MainRoomSettingComplete />
-    </>
-  );
+interface RepresentRoom {
+  room_id: number;
+  room_name: string;
+  all_avg_by_month_list: AvgByMonth[];
+}
+
+// 모든호실 연간 평균치
+export default async function Page({ searchParams }: dashboardPageType) {
+  const buildingName = searchParams && searchParams[QueryOptions.BuildingName];
+  const buildingId = searchParams && searchParams[QueryOptions.Id];
+
+  try {
+    const representRoomUrl = `/buildings/${buildingId}/rooms/represent`;
+    const representRoom = await fetchJsonData(representRoomUrl, {
+      cache: 'no-store',
+      method: 'GET',
+    });
+    const roomsYearScoreUrl = `/buildings/${buildingId}/my-rooms-year-score`;
+    const roomsYearScore: RepresentRoom[] = await fetchJsonData(roomsYearScoreUrl, {
+      cache: 'no-store',
+      method: 'GET',
+    });
+    return (
+      <>
+        <div className="flex flex-row gap-6 after:h-4 after:content-[''] desktop:flex-col">
+          {roomsYearScore &&
+            roomsYearScore.map(val => {
+              return (
+                <LineChartComp
+                  mainRoom={representRoom.id === val.room_id}
+                  key={val.room_id}
+                  val={val}
+                  roomId={val.room_id}
+                  roomName={val.room_name}
+                  buildingName={buildingName}
+                />
+              );
+            })}
+        </div>
+        {/* 모달 컴포넌트 */}
+        <MainRoomSetting />
+        <MainRoomSettingComplete />
+        <MainRoomSettingFailed />
+      </>
+    );
+  } catch (err) {
+    return null;
+  }
 }
