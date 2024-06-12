bbab04389dd4ae3d788f354c1538305f09a3e475
kano <ajvls98@gmail.com>
2024-06-12 16:42:25 +0900

    refactor: 요구사항에 맞게 컴포넌트 리펙터링

--- src/components/common/Atoms/badge/ScoreBadge.tsx
+++ src/components/common/Atoms/badge/ScoreBadge.tsx
@@ -4,13 +4,14 @@ import { cva, VariantProps } from 'class-variance-authority';
 
 import { Badge } from '@/components/ui/badge';
 import { cn } from '@/lib/utils';
+import { ScoreType } from '@/constants';
 
 const scoreBadgeVariants = cva('px-[14px] py-[5px] text-[14px] font-extrabold', {
   variants: {
     type: {
-      management: 'bg-Secondary-management text-Primary-management',
-      facility: 'bg-Secondary-facility text-Primary-facility',
-      claim: 'bg-Secondary-claim text-Primary-claim',
+      [ScoreType.MANAGEMENT]: 'bg-secondary-management text-primary-management',
+      [ScoreType.FACILITY]: 'bg-secondary-facility text-primary-facility',
+      [ScoreType.CLAIM]: 'bg-secondary-claim text-primary-claim',
     },
   },
 });
