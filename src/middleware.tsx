b4ab7c411d25dd60477c99f8f7271279be2a211c
Jihun <58257616+Lamyzm@users.noreply.github.com>
2024-06-19 13:46:34 +0900

    feat: 미들웨어로 유저 세션 role검증

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -7,6 +7,7 @@ import { CustomSession } from '@/types/auth';
 const matchersForAuth = [
   // '/dashboard/:path*', // '/dashboard', '/dashboard/1', '/dashboard/1/2'
   '/myaccount/:path+', // '/myaccount/1', '/myaccount/1/2/3'
+  '/dashboard/:path*', // '/dashboard', '/dashboard
   '/settings/:path', // '/settings/1'
   '/user-survey',
   '/user-survey/scores', // '/user-survey/scores', '/user-survey/scores?id=manage&num={scoreId}', '/user-survey/scores?id=done'
@@ -27,10 +28,10 @@ export async function middleware(request: NextRequest) {
   // 유저 정보
   const userInfo = (await auth()) as CustomSession;
 
-  // if (request.nextUrl.pathname.startsWith('/')) {
-  //   return NextResponse.redirect(new URL('/dashboard', request.url));
-  // }
-
+  // user role 이 소유자고 / 일때 dashboard로 리디렉션
+  if (userInfo && userInfo.role === 'OWNER' && pathname === '/') {
+    return NextResponse.redirect(new URL('/dashboard', request.url));
+  }
   // 유저토큰 없을시 join으로 리디렉션
   if (isMatch(pathname, matchersForAuth)) {
     return userInfo
@@ -38,6 +39,7 @@ export async function middleware(request: NextRequest) {
       : // 이전 url 위치 기억
         NextResponse.redirect(new URL(`/login?callbackUrl=${request.url}`, request.url));
   }
+
   // 유저토큰 있을시 로그인,회원가입 이동 차단
   if (pathname.startsWith('/login') || pathname.startsWith('/join')) {
     if (userInfo) {
